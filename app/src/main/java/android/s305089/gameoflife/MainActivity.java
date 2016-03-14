dbee2bbcc00f8b01d8cb209c62aa24829507e8ea
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-14 21:04:38 +0100

    Lagt inn QR-biblioteket

--- app/src/main/java/android/s305089/gameoflife/MainActivity.java
+++ app/src/main/java/android/s305089/gameoflife/MainActivity.java
@@ -5,6 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.Encoder;
+import com.google.zxing.qrcode.encoder.QRCode;
 
 import java.io.Serializable;
 
@@ -14,14 +20,24 @@ import java.io.Serializable;
 public class MainActivity extends Activity implements Serializable {
 
     private Button btnToQRCode;
+    private EditText editTexttoQR;
+    private QRCode qrCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         btnToQRCode = (Button) findViewById(R.id.btnToQRCode);
+        editTexttoQR = (EditText) findViewById(R.id.editTextToQR);
+
     }
 
-    public void textToQR(View view){
+    public void textToQR(View view) {
 
+        try {
+            qrCode = Encoder.encode(editTexttoQR.getText().toString(), ErrorCorrectionLevel.Q);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
